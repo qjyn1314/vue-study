f96b929ce9a0848d678b25d009c11a902668f912
qjyn1314 <qjyn1314@163.com>
2022-12-08 00:05:42 +0800

    添加学习记录

--- ajax/express_server.js
+++ ajax/express_server.js
@@ -1,6 +1,6 @@
 // 定义简单的服务接收器.
-const express_server = require('ajax/express_server');
-
+const express_server = require('express');
+// 启动命令: nodemon express_server.js
 const app = express_server();
 
 // req 是对请求报文的封装
@@ -15,7 +15,22 @@ app.get('/server', (req, res) => {
     res.setHeader('Access-Control-Allow-Origin','*');
 
     //设置响应体.
-    res.send("server 路径, 响应是- hello server.");
+    res.send("server 路径, get响应是- hello server.");
+});
+app.all('/server', (req, res) => {
+
+    //设置响应头 设置允许跨域
+    res.setHeader('Access-Control-Allow-Origin','*');
+    // 设置响应头的header信息可以是预定义的和自定义的请求头
+    res.setHeader('Access-Control-Allow-Headers','*');
+
+    const data = {
+        name:"张三"
+    }
+    let dataStr = JSON.stringify(data);
+    //设置响应体.
+    // res.send("server 路径, post响应是- hello server.");
+    res.send(dataStr);
 });
 
 app.listen(8012, () => {
