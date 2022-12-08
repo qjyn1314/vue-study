67d0790cc8c7f497bd1bd22f48da5e00892fe82e
wangjunming <qjyn1314@163.com>
2022-12-08 17:43:11 +0800

    添加学习记录

--- express_server/express_server.js
+++ express_server/express_server.js
@@ -1,7 +1,15 @@
+// 执行 npm i express   简单的服务.
+// 执行 npm i nodemon  文件更改的同时重启服务.
+// 执行  nodemon express_server.js 启动简单的服务,并且在文件更改的同时重启服务.
+
 // 定义简单的服务接收器.
 const express_server = require('express');
 // 启动命令: nodemon express_server.js
 const app = express_server();
+// 端口号
+app.listen(8012, () => {
+    console.log("服务已启动,8012端口监听中.....node")
+})
 
 // req 是对请求报文的封装
 // res 是对响应报文的封装
@@ -9,6 +17,7 @@ app.get('/', (req, res) => {
     res.send("hello express.");
 });
 
+// get请求
 app.get('/server', (req, res) => {
 
     //设置响应头 设置允许跨域
@@ -17,6 +26,7 @@ app.get('/server', (req, res) => {
     //设置响应体.
     res.send("server 路径, get响应是- hello server.");
 });
+//支持所有请求
 app.all('/server', (req, res) => {
 
     //设置响应头 设置允许跨域
@@ -33,6 +43,4 @@ app.all('/server', (req, res) => {
     res.send(dataStr);
 });
 
-app.listen(8012, () => {
-    console.log("服务已启动,8012端口监听中.....node")
-})
+
